69b08244146981849f42023ac9accf46b7f80486
toufique_py <munshitoufiqueahamed@gmail.com>
2026-01-27 13:20:26 +0530

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,59 @@
+"""tup=("a","b","c")
+print ("original tupple",tup)
+li=list(tup)
+li[0]='hi'
+li[1]='hello'
+tup=tuple(li)
+
+print(tup)
+print(tup[1:])
+tup=(24,)+tup[1:]
+print(tup)
+
+
+
+
+
+#new task
+a=[2,24,10,12,13,3,4,34,9,8]
+print(a[0:])
+print(a[1:9:2])
+print(a[-3])
+print(a[-1])
+print(a[9::-1])
+
+
+# string mutable or not
+st="munshitoufiqueahamed"
+print(st[0])
+st[0]=o
+print(st)"""
+
+
+#palindrome or not
+st="hello"
+if st[0::]==st[::-1]:
+    print("palindrome")
+else:
+    print("not")
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
